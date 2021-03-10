d0bae76b0f6f8817b4858e2d4072049b917ce290
Richard <zfanli@outlook.com>
2021-03-11 00:17:37 +0800

    Update plugin

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -2,18 +2,24 @@
 
 (function (factory) {
   // Register plugin with the name `_plugin` to avoid namespace pollution.
-  factory(jQuery, "_plugin");
+  // Notice that the 3rd argument is set to enable log info, or leave `null`
+  // to silence all logs.
+  factory(jQuery, "_plugin", console);
 
   // Scan and apply plugins by default.
   // Auto binding plugins to html elements which `data-plugin` is set.
-  // e.g. <div data-plugin="plugin-name" data-key="value" ... >...</div>
+  //    e.g. <div data-plugin="plugin-name" data-key="value" ... >...</div>
   // Except the plugin name, other attributes with `data-*` prefix will be
   // passed to the plugin as `props`.
   // * Think `props` as settings to plugins.
   $(document).ready(function () {
     $(document)._plugin();
   });
-})(function ($, bindName) {
+})(function ($, bindName, logger) {
+  /****************************************************************************
+   *                        Widget Registration                               *
+   ****************************************************************************/
+
   $.widget("custom." + bindName, {
     // Plugin list for management.
     _plugins: [],
@@ -117,6 +123,42 @@
     _create: function () {
       const elem = this.element;
       const options = this.options;
+      logging.log(elem);
+      this.options = {};
     },
+
+    // Response to data changes.
+    _setOptions: function () {},
+
+    _refresh: function () {},
+    _destroy: function () {},
+
+    _initProps: function () {},
+    _checkProps: function () {},
   });
+
+  /****************************************************************************
+   *                          Support Functions                               *
+   ****************************************************************************/
+
+  // For logging support, all logs will be suppress if logger is not set.
+  function logging() {}
+  Object.assign(logging, {
+    error: (...data) => logger && logger.error(...data),
+    info: (...data) => logger && logger.info(...data),
+    warn: (...data) => logger && logger.warn(...data),
+    log: (...data) => logger && logger.log(...data),
+    debug: (...data) => logger && logger.debug(...data),
+  });
+
+  /**
+   * Test if the object is the given type.
+   *
+   * @param {object} obj test object
+   * @param {string} type type string
+   * @returns test result
+   */
+  function isType(obj, type) {
+    return typeof obj === type;
+  }
 });
